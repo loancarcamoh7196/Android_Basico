ab116c16f06dea4515b509958c66518d367fd5cf
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-03-17 10:34:46 -0300

    [holaMundo] Muestra si alguien aprobo o reprobo con un msj emergente

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.miprimeraapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int matematicas=5, quimica= 5, fisica=5, promedio=0;
+
+        promedio = (matematicas + quimica + fisica ) /3;
+
+        if(promedio >= 6) {
+            Toast.makeText(this, "Aprobado", Toast.LENGTH_LONG);
+        } else if (promedio <=5) {
+            Toast.makeText(this, "Reprobo", Toast.LENGTH_LONG);
+        }
+
     }
 }
